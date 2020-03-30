fb1c2df1d4ea1db9b478b1fbe865e5a0a275e100
jurczakn <nickolas.jurczak@gmail.com>
2020-03-30 16:03:55 -0400

    added Dockerfile

--- PurchaseMicroservice/src/main/java/com/revature/purchase/PurchaseMicroserviceApplication.java
+++ PurchaseMicroservice/src/main/java/com/revature/purchase/PurchaseMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.revature.purchase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PurchaseMicroserviceApplication {
 
 	public static void main(String[] args) {
